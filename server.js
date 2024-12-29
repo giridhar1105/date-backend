9ba816eff3614b70867369808f879d14cb253b39
Giridhar <giridhaar1105@gmail.com>
2024-12-29 23:52:38 +0530

    little changes

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:9000",
+    origin: "http://localhost:7000",
     methods: ["GET", "POST"]
   }
 });
