77669077fa02e16642b873b4175beeeb72ebd731
Giridhar <giridhaar1105@gmail.com>
2024-12-29 23:52:10 +0530

    fixed http

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const http = require('p');
+const http = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
